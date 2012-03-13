9c1e429cda9c3a65f75b8b7a7b6fb15e6d446d44
Kurt Preston <kpreston@inventables.com>
2012-03-13 15:04:02 -0500

    Creating initial repository

---
+++ daddys_girl.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'daddys_girl'
+  s.version     = '0.2'
+  s.date        = '2020-03-13'
+  s.summary     = "[Object] Daddy's [Factory] Girl"
+  s.description = "Rubygem to provide object_daddy-like syntax for factory_girl"
+  s.authors     = ["Kurt Preston"]
+  s.email       = 'development@inventables.com'
+  s.files       = ["lib/object_daddy.rb"]
+  s.homepage    =
+    'https://github.com/inventables/daddys_girl'
+end
---
+++ lib/daddys_girl.rb
@@ -0,0 +1,54 @@
+class ActiveRecord::Base
+  class << self
+    def symbol
+      self.name.underscore.to_sym
+    end
+
+    def generate(attributes = {})
+      FactoryGirl.create(self.symbol, attributes)
+    end
+
+    def generate!(attributes = {})
+      FactoryGirl.create(self.symbol, attributes).tap do |obj|
+        raise obj.errors.inspect unless obj.errors.empty?
+      end
+    end
+
+    def spawn(attributes = {})
+      FactoryGirl.build(self.symbol, attributes)
+    end
+  end
+end
+
+class ActiveRecord::Associations::AssociationProxy
+  def target_class_symbol
+    self.symbol
+  end
+
+  def generate(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.create(target_class_symbol, attributes)
+  end
+
+  def generate!(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.create(target_class_symbol, attributes).tap do |obj|
+      raise obj.errors.inspect unless obj.errors.empty?
+    end
+  end
+
+  def spawn(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.build(target_class_symbol, attributes)
+  end
+
+  private
+  def owner_association
+    proxy_reflection.primary_key_name.to_sym
+  end
+
+  def association_attribute
+    {owner_association => proxy_owner.id}
+  end
+end
+
