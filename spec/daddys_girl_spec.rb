fe8bd65783e752d29a9ab533884c78bdd29a9565
Kurt Preston <kpreston@inventables.com>
2012-04-13 16:08:38 -0500

    Using FactoryGirl's ActiveRecord overriding methods

--- spec/daddys_girl_spec.rb
+++ spec/daddys_girl_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe DaddysGirl do
   it "successfully spawns" do
+    define_model('TestClass', {:name => :string})
+
+    FactoryGirl.define do
+      factory :test_class do
+        name 'Test Name'
+      end
+    end
+
     FactoryGirl.create(:test_class)
 
-    TestClass.spawn.name.should == "Test Name"
+    TestClass.bogusunder.name.should == "Test Name"
     TestClass.spawn.id.should be_nil
   end
 end
