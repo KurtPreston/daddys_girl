c6acd27a0dd86f0f3b38ca007a8b9fb455b623e7
Kurt Preston <kpreston@inventables.com>
2012-04-13 16:27:50 -0500

    Daddys girl now working with specs

--- lib/daddys_girl.rb
+++ lib/daddys_girl.rb
@@ -1,62 +1,57 @@
-module DaddysGirl
-  module ActiveRecordModel
-    class << self
-      def symbol
-        self.name.underscore.to_sym
-      end
+require 'active_record'
 
-      def generate(attributes = {})
-        FactoryGirl.create(self.symbol, attributes)
-      end
-
-      def generate!(attributes = {})
-        FactoryGirl.create(self.symbol, attributes).tap do |obj|
-          raise obj.errors.inspect unless obj.errors.empty?
-        end
-      end
-
-      def spawn(attributes = {})
-        FactoryGirl.build(self.symbol, attributes)
-      end
-    end
-  end
-end
-
-ActiveRecord::Base.send(:include, DaddysGirl::ActiveRecordModel) if defined?(ActiveRecord)
-
-
-module DaddysGirl
-  module AssociationModel
-    def target_class_symbol
-      self.symbol
+ActiveRecord::Base.class_eval do
+  class << self
+    def symbol
+      self.name.underscore.to_sym
     end
 
     def generate(attributes = {})
-      attributes = attributes.merge(association_attribute)
-      FactoryGirl.create(target_class_symbol, attributes)
+      FactoryGirl.create(self.symbol, attributes)
     end
 
     def generate!(attributes = {})
-      attributes = attributes.merge(association_attribute)
-      FactoryGirl.create(target_class_symbol, attributes).tap do |obj|
+      FactoryGirl.create(self.symbol, attributes).tap do |obj|
         raise obj.errors.inspect unless obj.errors.empty?
       end
     end
 
     def spawn(attributes = {})
-      attributes = attributes.merge(association_attribute)
-      FactoryGirl.build(target_class_symbol, attributes)
-    end
-
-    private
-    def owner_association
-      proxy_reflection.primary_key_name.to_sym
-    end
-
-    def association_attribute
-      {owner_association => proxy_owner.id}
+      FactoryGirl.build(self.symbol, attributes)
     end
   end
 end
 
-ActiveRecord::Associations::AssociationProxy.send(:include, DaddysGirl::AssociationModel) if defined?(ActiveRecord)
+
+ActiveRecord::Associations::AssociationProxy.class_eval do
+  def target_class_symbol
+    self.symbol
+  end
+
+  def generate(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.create(target_class_symbol, attributes)
+  end
+
+  def generate!(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.create(target_class_symbol, attributes).tap do |obj|
+      raise obj.errors.inspect unless obj.errors.empty?
+    end
+  end
+
+  def spawn(attributes = {})
+    attributes = attributes.merge(association_attribute)
+    FactoryGirl.build(target_class_symbol, attributes)
+  end
+
+  private
+  def owner_association
+    proxy_reflection.primary_key_name.to_sym
+  end
+
+  def association_attribute
+    {owner_association => proxy_owner.id}
+  end
+end
+
--- spec/daddys_girl_spec.rb
+++ spec/daddys_girl_spec.rb
@@ -1,18 +1,52 @@
 require 'spec_helper'
 
 describe DaddysGirl do
-  it "successfully spawns" do
-    define_model('TestClass', {:name => :string})
-
+  before(:all) do
     FactoryGirl.define do
       factory :test_class do
         name 'Test Name'
       end
     end
+  end
 
-    FactoryGirl.create(:test_class)
+  before(:each) do
+    define_model('TestClass', {:name => :string})
+  end
 
-    TestClass.bogusunder.name.should == "Test Name"
-    TestClass.spawn.id.should be_nil
+  describe "ActiveRecord::Base.spawn" do
+    it "creates a new object without saving" do
+      TestClass.spawn(:name => "Test Name").name.should == "Test Name"
+      TestClass.spawn.id.should be_nil
+    end
+  end
+
+  describe "ActiveRecord::Base.generate" do
+    context "if the object is valid" do
+      it "creates a new object and saves" do
+        TestClass.generate(:name => "Test Name").name.should == "Test Name"
+        TestClass.generate.id.should_not be_nil
+      end
+    end
+
+    context "if the object is not valid" do
+      it "creates a new object without saving" do
+        pending
+      end
+    end
+  end
+
+  describe "ActiveRecord::Base.generate!" do
+    context "if the object is valid" do
+      it "creates a new object and saves" do
+        TestClass.generate!(:name => "Test Name").name.should == "Test Name"
+        TestClass.generate!.id.should_not be_nil
+      end
+    end
+
+    context "if the object is not valid" do
+      it "throws an error" do
+        pending
+      end
+    end
   end
 end
